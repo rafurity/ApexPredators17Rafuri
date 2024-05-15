2e50c70d814ac2f2a9d902efc42d0ba39360caa7
rafurity <raflialfajari@gmail.com>
2024-05-15 13:15:08 +0700

    Initial Commit

---
+++ ApexPredators17Rafuri/ApexPredator.swift
@@ -0,0 +1,66 @@
+//
+//  ApexPredator.swift
+//  ApexPredators17Rafuri
+//
+//  Created by Macbook on 26/01/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ApexPredator: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let type: PredatorType
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    struct MovieScene: Decodable, Identifiable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
+enum PredatorType: String, Decodable, CaseIterable, Identifiable {
+    case all
+    case land
+    case air
+    case sea
+    
+    var id: PredatorType{
+        self
+    }
+    
+    var background: Color {
+        switch self {
+        case .land:
+                .brown
+        case .air:
+                .teal
+        case .sea:
+                .blue
+        case .all:
+                .black
+        }
+    }
+    var icon: String {
+        switch self {
+        case .all:
+            "square.stack.3d.up.fill"
+        case .land:
+            "leaf.fill"
+        case .air:
+            "wind"
+        case .sea:
+            "drop.fill"
+        }
+    }
+}
+
---
+++ ApexPredators17Rafuri/PredatorDetail.swift
@@ -0,0 +1,76 @@
+//
+//  PredatorDetail.swift
+//  ApexPredators17Rafuri
+//
+//  Created by Macbook on 15/05/24.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    let predator: ApexPredator
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView{
+                ZStack (alignment: .bottomTrailing) {
+                    // BG Image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // Dino Image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                }
+                VStack (alignment: .leading) {
+                    // Dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // Current location
+                    
+                    // Appears in
+                    Text("Appears In:")
+                        .font(.title3)
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("•" + movie)
+                            .font(.subheadline)
+                    }
+                    // Movie moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical,1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom,15)
+                    }
+                    // Link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
+            }
+            .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    PredatorDetail(predator: Predators().apexPredators[10])
+//        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
